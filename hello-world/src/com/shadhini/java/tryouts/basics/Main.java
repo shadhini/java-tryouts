d70d5e925ab4f7435d6a8e18506138840ee0e70e
shadhini <shadhini.jayatilake@gmail.com>
2024-09-09 13:01:41 +0530

    `Main` class and `main` method

---
+++ hello-world/src/com/shadhini/java/tryouts/basics/Main.java
@@ -0,0 +1,10 @@
+package com.shadhini.java.tryouts.basics;
+
+/**
+ * About `Main` class and `main` method
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
