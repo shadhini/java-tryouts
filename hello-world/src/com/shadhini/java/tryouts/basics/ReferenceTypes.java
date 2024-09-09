ba6e918d549f212b83c42fb4721530b96007d74a
shadhini <shadhini.jayatilake@gmail.com>
2024-09-09 14:50:08 +0530

    Java Reference Types

---
+++ hello-world/src/com/shadhini/java/tryouts/basics/ReferenceTypes.java
@@ -0,0 +1,14 @@
+package com.shadhini.java.tryouts.basics;
+
+import java.util.Date;
+
+/**
+ * About Java Reference Types
+ */
+public class ReferenceTypes {
+    public static void main(String[] args) {
+        byte age = 29;
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
