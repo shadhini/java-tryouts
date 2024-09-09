2f930e4f45559b84d3ca04c391cc6385b34a20bd
shadhini <shadhini.jayatilake@gmail.com>
2024-09-09 16:15:38 +0530

    Escape ('\') sequences for including special characters

---
+++ hello-world/src/com/shadhini/java/tryouts/basics/EscapeSequences.java
@@ -0,0 +1,28 @@
+package com.shadhini.java.tryouts.basics;
+
+/**
+ * About Escape ('\') sequences for including special characters.
+ */
+public class EscapeSequences {
+
+    public static void main(String[] args) {
+
+        // Double Quotes
+        System.out.println("------------Double Quotes-------------");
+        System.out.println("Hello \"Jane\"!");
+
+        // Backslash
+        System.out.println("------------Backslash-------------");
+        System.out.println("c:\\Windows\\...");
+
+        // New Lines \n
+        System.out.println("------------New lines-------------");
+        System.out.println("Line1\nLine2\nLine3");
+
+        // Tabs \t
+        System.out.println("------------Tabs-------------");
+        System.out.println("Tab1\tTab2\tTab3");
+
+
+    }
+}
