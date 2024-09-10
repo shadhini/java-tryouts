0d930c4f97492dce3daf170b770ab829a33e5f4d
shadhini <shadhini.jayatilake@gmail.com>
2024-09-10 13:42:13 +0530

    casting and type conversion

---
+++ basics/src/com/shadhini/java/tryouts/basics/CastingNTypeConversion.java
@@ -0,0 +1,35 @@
+package com.shadhini.java.tryouts.basics;
+
+/**
+ * About Java casting and type conversion.
+ */
+public class CastingNTypeConversion {
+
+    public static void main(String[] args) {
+
+        // Implicit Casting
+        // byte > short > int > long > float > double
+        short x = 1;
+        int y = x + 2;
+        System.out.println(y); // Output: 3
+
+        double r = 1.1;
+        double s = r + 2;
+        System.out.println(s); // Output: 3.1
+
+        // Explicit Casting
+        int t = (int) r + 2;
+        System.out.println(t); // Output: 3
+
+        String a = "1";
+        int b = Integer.parseInt(a) + 2;
+        System.out.println(b); // Output: 3
+
+        String c = "1.1";
+        // int d = Integer.parseInt(c) + 2; // Output: java.lang.NumberFormatException
+        // System.out.println(d);
+        double e = Double.parseDouble(c) + 2;
+        System.out.println(e); // Output: 3.1
+
+    }
+}
