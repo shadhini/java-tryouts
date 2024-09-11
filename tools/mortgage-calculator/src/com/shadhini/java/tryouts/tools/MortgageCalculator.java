7845dd49fe1c6bc1b7396766ebc615ab65247f96
shadhini <shadhini.jayatilake@gmail.com>
2024-09-11 14:03:09 +0530

    add input validations to Mortgage Calculator

--- tools/mortgage-calculator/src/com/shadhini/java/tryouts/tools/MortgageCalculator.java
+++ tools/mortgage-calculator/src/com/shadhini/java/tryouts/tools/MortgageCalculator.java
@@ -5,27 +5,54 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
 
-    //TODO: Input Validation
+    //TODO: Make code more readable and understandable by breaking it down to small parts
 
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+        final int MIN_PRINCIPAL = 1000;
+        final int MAX_PRINCIPAL = 1_000_000;
+        final byte MIN_PERIOD = 1;
+        final byte MAX_PERIOD = 30;
+        final byte MIN_INTEREST_RATE = 1;
+        final byte MAX_INTEREST_RATE = 30;
 
+        int principal = 0;
+        byte period = 0;
+        int numOfPayments = 0;
+        float annualInterestRate = 0;
+        float monthlyInterestRate = 0;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= MIN_PRINCIPAL && principal <= MAX_PRINCIPAL)
+                break;
+            System.out.println("Enter a number between " + MIN_PRINCIPAL + " and " + MAX_PRINCIPAL + ".");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate >= MIN_INTEREST_RATE && annualInterestRate <= MAX_INTEREST_RATE) {
+                monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between " + MIN_INTEREST_RATE + " and " + MAX_INTEREST_RATE + ".");
+        }
 
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte(); // Maximum would be 30
-        int numOfPayments = period / MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Period (Years): ");
+            period = scanner.nextByte(); // Maximum would be 30
+            if (period >= MIN_PERIOD && period <= MAX_PERIOD) {
+                numOfPayments = period * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between " + MIN_PERIOD + " and " + MAX_PERIOD + ".");
+        }
 
         // Mortgage calculation
-
         double power = Math.pow(1  + monthlyInterestRate, numOfPayments);
         double mortgage = (principal * monthlyInterestRate * power) / (power - 1);
         System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
