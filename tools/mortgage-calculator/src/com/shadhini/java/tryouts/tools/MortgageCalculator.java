0e5c63cf5d9d9e590a05fd7d461a03d3130647e8
shadhini <shadhini.jayatilake@gmail.com>
2024-09-11 17:54:37 +0530

    refactoring mortgage calculator

--- tools/mortgage-calculator/src/com/shadhini/java/tryouts/tools/MortgageCalculator.java
+++ tools/mortgage-calculator/src/com/shadhini/java/tryouts/tools/MortgageCalculator.java
@@ -5,41 +5,47 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
 
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
     public static void main(String[] args) {
 
-        final int MIN_PRINCIPAL = 1000;
-        final int MAX_PRINCIPAL = 1_000_000;
-        final byte MIN_PERIOD = 1;
-        final byte MAX_PERIOD = 30;
-        final byte MIN_INTEREST_RATE = 1;
-        final byte MAX_INTEREST_RATE = 30;
-
-        int principal = (int) readNumber("Principal($1K - $1M): ", MIN_PRINCIPAL, MAX_PRINCIPAL);
+        int principal = (int) readNumber("Principal($1K - $1M): ", 1000, 1_000_000);
         float annualInterestRate = (float) readNumber(
-                "Annual Interest Rate: ", MIN_INTEREST_RATE, MAX_INTEREST_RATE);
-        byte period = (byte) readNumber("Period (Years): ", MIN_PERIOD, MAX_PERIOD);
+                "Annual Interest Rate: ", 1, 30);
+        byte period = (byte) readNumber("Period (Years): ", 1, 30);
 
-        double mortgage = calculateMortgage(principal, annualInterestRate, period);
-
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        displayMortgage(principal, annualInterestRate, period);
+        displayPaymentSchedule(principal, annualInterestRate, period);
     }
 
     /**
-     * Calculate mortgage
+     * Display Mortgage
      * @param principal principal amount of the loan
      * @param annualInterestRate annual interest rate of the loan
      * @param period loan payment period in years
-     * @return monthly payment as a double
      */
-    public static double calculateMortgage(int principal, float annualInterestRate, byte period) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+    private static void displayMortgage(int principal, float annualInterestRate, byte period) {
+        double mortgage = calculateMortgage(principal, annualInterestRate, period);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("______________________");
+        System.out.println("Monthly Payments: " + NumberFormat.getCurrencyInstance().format(mortgage));
+    }
 
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
-        short numOfPayments = (short) (period * MONTHS_IN_YEAR);
-
-        double intermediateCalc = Math.pow(1  + monthlyInterestRate, numOfPayments);
-        return  (principal * monthlyInterestRate * intermediateCalc) / (intermediateCalc - 1);
+    /**
+     * Display Payment Schedule
+     * @param principal principal amount of the loan
+     * @param annualInterestRate annual interest rate of the loan
+     * @param period loan payment period in years
+     */
+    private static void displayPaymentSchedule(int principal, float annualInterestRate, byte period) {
+        double[] paymentSchedule = calculatePaymentSchedule(principal, annualInterestRate, period);
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("______________________");
+        for (double payment: paymentSchedule)
+            System.out.println(NumberFormat.getCurrencyInstance().format(payment));
     }
 
     /**
@@ -51,7 +57,7 @@ public class MortgageCalculator {
      */
     public static double readNumber(String prompt, double min, double max) {
         Scanner scanner = new Scanner(System.in);
-        double value = 0;
+        double value;
         while (true) {
             System.out.print(prompt);
             value = scanner.nextDouble();
@@ -60,5 +66,41 @@ public class MortgageCalculator {
             System.out.println("Enter a number between " + min + " and " + max + ".");
         }
         return value;
+    }
+
+    /**
+     * Calculate mortgage
+     * @param principal principal amount of the loan
+     * @param annualInterestRate annual interest rate of the loan
+     * @param period loan payment period in years
+     * @return monthly payment as a double
+     */
+    public static double calculateMortgage(int principal, float annualInterestRate, byte period) {
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        short numOfPayments = (short) (period * MONTHS_IN_YEAR);
+
+        double intermediateCalc = Math.pow(1  + monthlyInterestRate, numOfPayments);
+        return  (principal * monthlyInterestRate * intermediateCalc) / (intermediateCalc - 1);
+    }
+
+    /**
+     * Calculate the whole Payment Schedule
+     * @param principal principal amount of loan
+     * @param annualInterestRate annual interest rate of loan
+     * @param period loan payment period in years
+     * @return loan balance at the end of each month as a list of doubles
+     */
+    public static double[] calculatePaymentSchedule(int principal, float annualInterestRate, byte period) {
+
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        short numOfPayments = (short) (period * MONTHS_IN_YEAR);
+
+        double[] paymentSchedule = new double[numOfPayments];
+
+        double intermediateCalc = Math.pow(1  + monthlyInterestRate, numOfPayments);
+        for (int i = 1; i <= numOfPayments; i++)
+            paymentSchedule[i-1] =
+                    principal * (intermediateCalc - Math.pow((1 + monthlyInterestRate), i)) / (intermediateCalc - 1);
+        return paymentSchedule;
     }
 }
