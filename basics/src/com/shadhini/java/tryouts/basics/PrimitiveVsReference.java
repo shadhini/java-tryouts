32cd54499f12ceff861f103d42cd48656f3700ed
shadhini <shadhini.jayatilake@gmail.com>
2024-09-21 17:52:04 +0530

    update PrimitiveVsReference

--- basics/src/com/shadhini/java/tryouts/basics/PrimitiveVsReference.java
+++ basics/src/com/shadhini/java/tryouts/basics/PrimitiveVsReference.java
@@ -25,8 +25,10 @@ public class PrimitiveVsReference {
         Point point1 = new Point(1, 1);
         Point point2 = point1;
         point1.x = 3;
+        System.out.println(point1);
         System.out.println(point2);
         /* Output:
+        java.awt.Point[x=3,y=1]
         java.awt.Point[x=3,y=1]
          */
 
