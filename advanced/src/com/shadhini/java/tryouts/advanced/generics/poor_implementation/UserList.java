bf657365cda7a1e294fb114da044194e9558a6f2
shadhini <shadhini.jayatilake@gmail.com>
2024-10-09 19:54:34 +0530

    generics poor implementation

---
+++ advanced/src/com/shadhini/java/tryouts/advanced/generics/poor_implementation/UserList.java
@@ -0,0 +1,11 @@
+package com.shadhini.java.tryouts.advanced.generics.poor_implementation;
+
+import com.shadhini.java.tryouts.advanced.generics.User;
+
+public class UserList {
+
+    private User[] users = new User[100];
+    private int count;
+
+    // add methods to add items and remove here too
+}
