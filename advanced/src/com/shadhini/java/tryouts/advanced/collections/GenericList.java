c6174c0e9cb260881b38fd04ac99a0d3bf969307
shadhini <shadhini.jayatilake@gmail.com>
2024-10-13 09:36:46 +0530

    Iterator Interface

--- advanced/src/com/shadhini/java/tryouts/advanced/collections/GenericList.java
+++ advanced/src/com/shadhini/java/tryouts/advanced/collections/GenericList.java
@@ -16,6 +16,32 @@ public class GenericList<T> implements Iterable<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        // Here we are creating a ListIterator for the current GenericList.
+        return new ListIterator(this);
+    }
+
+    // The `T` we define here in Iterator<T> is the same `T` we used in GenericList<T>
+    // Since we want to iterate over the same type parameter as declared on top
+    private class ListIterator implements Iterator<T> {
+
+        //Here we want to iterate over a GenericList<T>; so it should be passed to the constructor
+        private GenericList<T> list;
+        private int index;
+
+        public ListIterator(GenericList<T> list) {
+            this.list = list;
+            // `list.items` is accessible here: it's okay since, this class is part of implementation of a GenericList<T>
+            // Since these details are not exposed to outside
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < list.count);
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
     }
 }
